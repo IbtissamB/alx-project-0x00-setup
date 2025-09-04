7df1524b8d3866dbda633270b263bb697add310f
IbtissamB <ibtissam.bahaj@hotmail.com>
2025-09-04 07:28:37 +0300

    added button component

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,4 +1,5 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button";
 
 const Landing: React.FC =  () => {
   return (
@@ -6,6 +7,9 @@ const Landing: React.FC =  () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
       <Card />
+      <Button title="Small Rounded" styles="px-3 py-1 text-sm rounded-sm bg-blue-500" />
+      <Button title="Medium Rounded" styles="px-4 py-2 text-base rounded-md bg-green-500" />
+      <Button title="Large Full" styles="px-6 py-3 text-lg rounded-full bg-purple-500" />
     </div>
   )
 }
